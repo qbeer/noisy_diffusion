d7c72cdcea6f1f4ca1887e38b6c78859359c75a5
Olar Alex <olaralex666@gmail.com>
2023-02-25 21:27:55 +0100

    updates

--- sample.py
+++ sample.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import argparse
+
 import imageio.v2 as imageio
 import matplotlib.pyplot as plt
 import torch
@@ -11,57 +13,64 @@ from noise_diffusion.model import Unet
 seed_everything(42, workers=True)
 
 
-def run():
-    model = Unet.load_from_checkpoint(
-        'lightning_logs/noisy_diffusion/epoch=97-valid_loss=48.022.ckpt',
+def run(args):
+    model = Unet(
+        backbone=args.backbone,
+        in_chans=1 if args.dataset == 'mnist' else 3,
+        num_classes=1 if args.dataset == 'mnist' else 3,
     )
+    chkpt = torch.load(args.chkpt)
+    model.load_state_dict(chkpt['state_dict'])
     model = model.to(device='cuda:0')
     model = model.eval()
 
-    def run_sampling(T=100, L=10, sigma_1=1, sigma_10=0.01, eps=2e-5):
+    def run_sampling(T=50, L=10, sigma_1=1, sigma_10=0.01, eps=2e-5):
         samples = []
-
         x = torch.rand(
-            size=(16, 1, 32, 32),
+            size=(36, 1 if args.dataset == 'mnist' else 3, 32, 32),
             dtype=torch.float32,
         ).to(device='cuda:0')
-        samples.append(x.cpu().detach().numpy())
+        samples.append(x.cpu().permute(0, 2, 3, 1).detach().numpy())
         stds = torch.tensor(
             [sigma_1 * ((sigma_10/sigma_1)**(1/(L-1)))**i for i in range(L)],
         )
         for i in range(L):
             alpha = torch.tensor(eps).to(device='cuda:0') * \
                 stds[i]**2 / stds[-1]**2
-            i = torch.tensor([i], dtype=torch.long).repeat(16).view(16, 1)
+            i = torch.tensor([i], dtype=torch.long).repeat(36).view(36, 1)
             i = i.to(device='cuda:0')
             for _ in range(T):
-                z = torch.randn(size=(16, 1, 32, 32)).to(device='cuda:0')
+                z = torch.randn(
+                    size=(
+                        36, 1 if args.dataset ==
+                        'mnist' else 3, 32, 32,
+                    ),
+                ).to(device='cuda:0')
                 x = x + alpha / 2. * \
                     model.forward(x, i) + torch.sqrt(alpha) / 2 * z  # trick?
                 x = torch.clip(x, 0., 1.)
-                samples.append(x.cpu().detach().numpy())
+                samples.append(x.cpu().permute(0, 2, 3, 1).detach().numpy())
         return samples
 
     with torch.no_grad():
         samples = run_sampling()
 
     with imageio.get_writer(
-        'samples/model_samples.gif',
+        f'samples/{args.dataset}_model_samples.gif',
         mode='I', duration=0.02,
     ) as writer:
-        for ind in tqdm(list(range(0, len(samples), 3))):
+        for ind in tqdm(list(range(0, len(samples), 5))):
             sample = samples[ind]
             fig, axes = plt.subplots(
-                4, 4, sharex=True, sharey=True, figsize=(12, 12),
+                6, 6, sharex=True, sharey=True, figsize=(15, 15),
             )
             axes = axes.flatten()
             for img, ax in zip(sample, axes):
-                img = img.reshape(32, 32)
                 ax.imshow(img, vmin=0, vmax=1, interpolation=None)
                 ax.set_xticks([])
                 ax.set_yticks([])
             fig.tight_layout()
-            plt.savefig(f'/tmp/{ind}.png', dpi=15)
+            plt.savefig(f'/tmp/{ind}.png', dpi=50)
             plt.close()
 
             image = imageio.imread(f'/tmp/{ind}.png')
@@ -69,4 +78,14 @@ def run():
 
 
 if __name__ == '__main__':
-    exit(run())
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--dataset', choices=['mnist', 'cifar10'], default='mnist',
+    )
+    parser.add_argument(
+        '--backbone', choices=['resnet50', 'resnet101', 'resnet152'],
+        default='resnet50',
+    )
+    parser.add_argument('--chkpt', required=True, type=str)
+    args = parser.parse_args()
+    exit(run(args))
--- train.py
+++ train.py
@@ -1,59 +1,100 @@
+from __future__ import annotations
+
 import argparse
 
-import torch
-
 import pytorch_lightning as pl
+import torch
+from pytorch_lightning import seed_everything
+from pytorch_lightning.callbacks import Callback
+from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks import Callback
 
 from noise_diffusion.model import Unet
-from noise_diffusion.dataset import NoisyMNIST
-
-from pytorch_lightning import seed_everything
+from noise_diffusion.noisy_cifar10 import NoisyCIFAR10
+from noise_diffusion.noisy_mnist import NoisyMNIST
 
 seed_everything(42, workers=True)
 
-def train(args):
-    model = Unet()
 
-    train_ds = NoisyMNIST(train=True)
-    val_ds = NoisyMNIST(train=False)
+def train(args):
+    model = Unet(
+        backbone=args.backbone,
+        in_chans=1 if args.dataset == 'mnist' else 3,
+        num_classes=1 if args.dataset == 'mnist' else 3,
+    )
+
+    if args.dataset == 'mnist':
+        train_ds = NoisyMNIST(train=True)
+        val_ds = NoisyMNIST(train=False)
+    else:
+        train_ds = NoisyCIFAR10(train=True)
+        val_ds = NoisyCIFAR10(train=False)
 
     train_dl = torch.utils.data.DataLoader(
         train_ds, batch_size=args.batch_size,
         shuffle=True, num_workers=5,
-        pin_memory=False)
+        pin_memory=False,
+    )
 
     val_dl = torch.utils.data.DataLoader(
         val_ds, batch_size=args.batch_size,
         shuffle=False, num_workers=5,
-        pin_memory=False)
+        pin_memory=False,
+    )
 
-    wandb_logger = WandbLogger(project='noise-diffusion', entity='elte-ai4covid', 
-        save_dir='./lightning_logs/')
+    wandb_logger = WandbLogger(
+        project='noise-diffusion', entity='elte-ai4covid',
+        save_dir='./lightning_logs/',
+    )
+
+    lr_monitor = LearningRateMonitor(logging_interval='epoch')
 
     class LogPredictionsCallback(Callback):
-        def _sample(self, pl_module, T=100, L=10, sigma_1=1, sigma_10=0.01, eps=2e-5):
-            x = torch.rand(size=(16, 1, 32, 32)).to(device='cuda:0')
-            stds = torch.tensor([ sigma_1 * ( (sigma_10/sigma_1)**(1/(L-1)) )**i for i in range(L) ])
+        def _sample(
+            self, pl_module,
+            T=100, L=10, sigma_1=1, sigma_10=0.01, eps=2e-5,
+        ):
+            x = torch.rand(
+                size=(
+                    16, 1 if args.dataset ==
+                    'mnist' else 3, 32, 32,
+                ),
+            ).to(device='cuda:0')
+            stds = torch.tensor(
+                [
+                    sigma_1 * ((sigma_10/sigma_1)**(1/(L-1)))
+                    ** i for i in range(L)
+                ],
+            )
             for i in range(L):
-                alpha = torch.tensor(eps).to(device='cuda:0') * stds[i]**2 / stds[-1]**2 
+                alpha = torch.tensor(eps).to(
+                    device='cuda:0',
+                ) * stds[i]**2 / stds[-1]**2
                 i = torch.tensor([i], dtype=torch.long).repeat(16).view(16, 1)
                 i = i.to(device='cuda:0')
                 for _ in range(T):
-                    z = torch.randn(size=(16, 1, 32, 32)).to(device='cuda:0')
-                    x = x + alpha / 2. * pl_module.forward(x, i) + torch.sqrt(alpha) * z
+                    z = torch.randn(
+                        size=(16, 1 if args.dataset == 'mnist' else 3, 32, 32),
+                    ).to(
+                        device='cuda:0',
+                    )
+                    x = x + alpha / 2. * \
+                        pl_module.forward(x, i) + torch.sqrt(alpha) / 2. * z
                     x = torch.clip(x, 0., 1.)
-            return x.cpu().detach().numpy()
-
+            return x.cpu().detach().permute(0, 2, 3, 1).numpy()
 
         def on_validation_batch_end(
-            self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+            self, ph1, pl_module, ph2, ph3, batch_idx, ph4,
+        ):
             """Called when the validation batch ends."""
             if batch_idx == 0:
                 samples = self._sample(pl_module)
-                wandb_logger.log_image(key='sample_images', images=[sample[0] for sample in samples])
+                wandb_logger.log_image(
+                    key='sample_images', images=[
+                        sample for sample in samples
+                    ],
+                )
 
     log_pred_callback = LogPredictionsCallback()
 
@@ -62,27 +103,38 @@ def train(args):
         dirpath='./lightning_logs/noisy_diffusion/',
         save_top_k=3,
         mode='min',
-        filename='{epoch:02d}-{valid_loss:.3f}')
-    
-    trainer = pl.Trainer(max_epochs=args.epochs,
-                         devices=1,
-                         accelerator='gpu',
-                         logger=wandb_logger,
-                         val_check_interval=1.0,
-                         min_epochs=args.epochs,
-                         callbacks=[
-                            checkpoint_callback,
-                            log_pred_callback
-                         ],
-                         deterministic=True,
-                         gradient_clip_val=.99, gradient_clip_algorithm="value")
+        filename=f'{args.dataset}-' + '{epoch:02d}-{valid_loss:.3f}',
+    )
+
+    trainer = pl.Trainer(
+        max_epochs=args.epochs,
+        devices=1,
+        accelerator='gpu',
+        logger=wandb_logger,
+        val_check_interval=1.0,
+        min_epochs=args.epochs,
+        callbacks=[
+            checkpoint_callback,
+            log_pred_callback,
+            lr_monitor,
+        ],
+        deterministic=True,
+        gradient_clip_val=.99, gradient_clip_algorithm='value',
+    )
 
     trainer.fit(model, train_dl, val_dl)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--epochs', type=int, default=100, required=False)
+    parser.add_argument(
+        '--dataset', choices=['mnist', 'cifar10'], default='mnist',
+    )
+    parser.add_argument(
+        '--backbone', choices=['resnet50', 'resnet152'], default='resnet50',
+    )
 
     args = parser.parse_args()
     train(args)
